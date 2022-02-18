906acbd6186b167d61a2ea8416b3e89a06cfa474
xp <a.xp@live.com>
2022-02-18 22:25:32 +0800

    add my build history as windows/*.sh

---
+++ windows/msys-ucrt-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm configure
+# autoheader
+# autoconf
+windows/MYconfigure # (--help to see options)
+make
---
+++ windows/msys-ucrt-init.sh
@@ -0,0 +1,9 @@
+pacman -Syu \
+    autoconf automake pkgconf \
+    git \
+    mingw-w64-ucrt-x86_64-binutils \
+    mingw-w64-ucrt-x86_64-clang \
+    mingw-w64-ucrt-x86_64-make \
+    mingw-w64-ucrt-x86_64-mbedtls \
+    mingw-w64-ucrt-x86_64-pcre \
+    mingw-w64-ucrt-x86_64-zlib \
